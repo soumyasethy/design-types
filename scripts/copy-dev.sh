002728a9bc7ee343bc994a716a49b5c014a2236f
soumyasethy <soumya.sethy@voltmoney.in>
2022-10-05 19:05:36 +0530

    update copy scripts for MF

--- scripts/copy-dev.sh
+++ scripts/copy-dev.sh
@@ -7,11 +7,17 @@ APP_PATH="../volt-app/node_modules/@voltmoney/types"
 mkdir -p $APP_PATH/"lib" && echo "👉 Link -> $APP_PATH/lib ✅ " #&& cd .. #uncomment when run local
 [ -d $APP_PATH ] && cp -R $BUILD_PATH $APP_PATH && echo "👉 Copy @volt/types -> Volt-App ✅ "
 
-# 1.Copy to DC/component ->  only for dev use
+# 2.Copy to DC/component ->  only for dev use
 APP_PATH="../design-component/node_modules/@voltmoney/types"
 [ -d $APP_PATH ] && rm -rf $APP_PATH/"lib" && echo "👉 Clear -> $APP_PATH/lib ✅ "
 mkdir -p $APP_PATH/"lib" && echo "👉 Link -> $APP_PATH/lib ✅ " #&& cd .. #uncomment when run local
 [ -d $APP_PATH ] && cp -R $BUILD_PATH $APP_PATH && echo "👉 Copy @volt/types -> DC/component ✅ "
+
+# 3.Copy to MF ->  only for dev use
+APP_PATH="../micro-frontend/node_modules/@voltmoney/types"
+[ -d $APP_PATH ] && rm -rf $APP_PATH/"lib" && echo "👉 Clear -> $APP_PATH/lib ✅ "
+mkdir -p $APP_PATH/"lib" && echo "👉 Link -> $APP_PATH/lib ✅ " #&& cd .. #uncomment when run local
+[ -d $APP_PATH ] && cp -R $BUILD_PATH $APP_PATH && echo "👉 Copy @volt/types -> Volt-App ✅ "
 
 
 #NO_CODE_PATH="../no-code-platform/node_modules/@volt/types"
