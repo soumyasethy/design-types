a7dc164f2f4646b8020ebb32bb993f8b208d8f39
soumyasethy <soumya.sethy@voltmoney.in>
2022-10-06 12:36:55 +0530

    refactor

--- scripts/copy-dev.sh
+++ scripts/copy-dev.sh
@@ -1,6 +1,5 @@
 BUILD_PATH="lib"
 echo "Copy @volt/types ⏳ "
-
 # 1.Copy to Volt App ->  only for dev use
 APP_PATH="../volt-app/node_modules/@voltmoney/types"
 [ -d $APP_PATH ] && rm -rf $APP_PATH/"lib" && echo "👉 Clear -> $APP_PATH/lib ✅ "
@@ -19,14 +18,12 @@ pwd
 APP_PATH="../micro-frontend/node_modules/@voltmoney/types"
 [ -d $APP_PATH ] && rm -rf $APP_PATH/"lib" && echo "👉 Clear -> $APP_PATH/lib ✅ "
 mkdir -p $APP_PATH/"lib" && echo "👉 Link -> $APP_PATH/lib ✅ " #&& cd .. #uncomment when run local
-pwd
 [ -d $APP_PATH ] && cp -R $BUILD_PATH $APP_PATH && echo "👉 Copy @volt/types -> MF ✅ "
 
 # 4.Copy to Platform ->  only for dev use
-APP_PATH="../design-paltform/node_modules/@voltmoney/types"
+APP_PATH="../design-platform/node_modules/@voltmoney/types"
 [ -d $APP_PATH ] && rm -rf $APP_PATH/"lib" && echo "👉 Clear -> $APP_PATH/lib ✅ "
 mkdir -p $APP_PATH/"lib" && echo "👉 Link -> $APP_PATH/lib ✅ " #&& cd .. #uncomment when run local
-pwd
 [ -d $APP_PATH ] && cp -R $BUILD_PATH $APP_PATH && echo "👉 Copy @volt/types -> Platform ✅ "
 
 
