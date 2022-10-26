bbc468443026196aa355d8a6258926a75d80a3f5
soumyasethy <soumya.sethy@voltmoney.in>
2022-10-26 10:05:26 +0530

    refactor

--- scripts/copy-watcher.sh
+++ scripts/copy-watcher.sh
@@ -3,8 +3,8 @@
 # Trigger on rebuild of lib
 # Copy to Volt App ->  only for dev use
 APP_PATH="../volt-app/node_modules/@voltmoney/types"
-#NO_CODE_PATH="../no-code-platform/node_modules/@volt/types"
-[ -d $APP_PATH ] && cp -R $1 $APP_PATH/$1 && echo "👉 Watcher Copy @volt/types -> Volt-App ✅ "
+NO_CODE_PATH="../no-code-platform/node_modules/@voltmoney/types"
+[ -d $APP_PATH ] && cp -R $1 $APP_PATH/$1 && echo "👉 Watcher Copy @voltmoney/types -> Volt-App ✅ "
 APP_PATH="../design-component/node_modules/@voltmoney/types"
-[ -d $APP_PATH ] && cp -R $1 $APP_PATH/$1 && echo "👉 Watcher Copy @volt/types -> DC/component ✅ "
-#[ -d $NO_CODE_PATH ] && cp -R $1 $NO_CODE_PATH/$1 && echo "👉 Watcher Copy @volt/micro-frontend -> NO_CODE_PATH ✅ "
+[ -d $APP_PATH ] && cp -R $1 $APP_PATH/$1 && echo "👉 Watcher Copy @voltmoney/types -> DC/component ✅ "
+[ -d $NO_CODE_PATH ] && cp -R $1 $NO_CODE_PATH/$1 && echo "👉 Watcher Copy @voltmoney/micro-frontend -> NO_CODE_PATH ✅ "
